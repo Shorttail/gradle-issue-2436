1305ebd16c3116ec957516b608cf4622c01695d5
Casper Færgemand <shorttail@hotmail.com>
2017-08-18 20:53:49 -0500

    Initial commit.

---
+++ src/integrationtest/java/com/example/AIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+public class AIntegrationTest {
+	
+	@Test
+	public void failure() {
+		A a = new A();
+	}
+	
+}
---
+++ src/test/java/com/example/ATest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+
+public class ATest {
+	
+	@Test
+	public void success() {
+		A a = new A();
+	}
+	
+}
